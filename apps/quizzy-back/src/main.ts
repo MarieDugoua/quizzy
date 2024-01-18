4f4dd702521417c1edb1455c8ba1d29653b76234
hugo <piai.hugo@gmail.com>
2024-01-18 11:56:34 +0100

    Fix front cors

--- apps/quizzy-back/src/main.ts
+++ apps/quizzy-back/src/main.ts
@@ -12,6 +12,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors({
+    allowedHeaders: ['Location', 'authorization', 'content-type'],
+    exposedHeaders: ['Location'],
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
